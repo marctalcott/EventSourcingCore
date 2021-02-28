271351e524dc90eb7224478e37c694805e12518f
marctalcott@gmail.com <marctalcott@gmail.com>
2021-02-28 09:25:20 -0500

    Add Customer
    Added CustomerAggregate, events, commands, handlers, views, projections and tests. I didn't add to API, but you can run with the unit tests.

--- Ezley.Testing/TestConfig.cs
+++ Ezley.Testing/TestConfig.cs
@@ -8,28 +8,28 @@ namespace Ezley.Testing
         public long StartTimeEpoch = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
         
         public readonly bool UseInMemoryEventStore = false;
-        public readonly string EventsEndpointUri = "https://esdemo1.documents.azure.com:443/";
+        public readonly string EventsEndpointUri = "https://[yourdb].documents.azure.com:443/";
         public readonly string EventsDatabase = "esdemo";
         public readonly string EventsAuthKey = 
-            "aENKTvufKynrTcKWbVJH8iMIOR7KIfeh6YqFzN7epC5y3Ad0vVsHNp3UtcSELVL77UCfsTfdxlA12g1ola5PMA==";
+            "[yourkey]";
         public readonly string EventContainer = "events";
         
-        public readonly string LeasesEndpointUri = "https://esdemo1.documents.azure.com:443/";
+        public readonly string LeasesEndpointUri = "https://[yourdb].documents.azure.com:443/";
         public readonly string LeasesDatabase = "esdemo";
         public readonly string LeasesAuthKey = 
-            "aENKTvufKynrTcKWbVJH8iMIOR7KIfeh6YqFzN7epC5y3Ad0vVsHNp3UtcSELVL77UCfsTfdxlA12g1ola5PMA==";
+            "[yourkey]";
         public readonly string LeasesContainer = "leases";
         
-        public readonly string ViewsEndpointUri = "https://esdemo1.documents.azure.com:443/";
+        public readonly string ViewsEndpointUri = "https://[yourdb].documents.azure.com:443/";
         public readonly string ViewsDatabase = "esdemo";
         public readonly string ViewsAuthKey = 
-            "aENKTvufKynrTcKWbVJH8iMIOR7KIfeh6YqFzN7epC5y3Ad0vVsHNp3UtcSELVL77UCfsTfdxlA12g1ola5PMA==";
+            "[yourkey]";
         public readonly string ViewsContainer = "views";
         
-        public readonly string SnapshotsEndpointUri = "https://esdemo1.documents.azure.com:443/";
+        public readonly string SnapshotsEndpointUri = "https://[yourdb].documents.azure.com:443/";
         public readonly string SnapshotsDatabase = "esdemo";
         public readonly string SnapshotsAuthKey = 
-            "aENKTvufKynrTcKWbVJH8iMIOR7KIfeh6YqFzN7epC5y3Ad0vVsHNp3UtcSELVL77UCfsTfdxlA12g1ola5PMA==";
+            "[yourkey]";
         public readonly string SnapshotsContainer = "snapshots";
         
         
